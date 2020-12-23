c0e1622a5ebf31fee1311903b41e87424322fb2d
skeady <skeady@gmail.com>
2020-12-23 14:56:45 +0000

    updating package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name="MaliciousDetection",
+    version="1.0",
+    packages=find_packages(where="src", exclude=("test",)),
+    package_dir={"": "src"},
+    entry_points="""\
+    [console_scripts]
+    malware-detection = MaliciousDetection.cli:cli
+    """
+)
