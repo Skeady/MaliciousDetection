ac0598b9167f79ac5155babd3fb891db90d6eeb1
SKeady <test@gmail.com>
2021-01-03 17:41:42 +0000

    updating package

--- install.sh
+++ install.sh
@@ -9,4 +9,4 @@ read api_key
 echo -e KEY=$api_key >> .env
 python3 setup.py develop
 GREEN= '\033[0;32m'
-echo -e "\n\e[32mMalicious-detection is now installed run malicious-detection --help for available commands\n\n"
+echo -e "\n\e[32mMalicious-detection is now installed run \e[1m\e[4mmalware-detection --help\e[0m\e[32m for available commands\n\n"
